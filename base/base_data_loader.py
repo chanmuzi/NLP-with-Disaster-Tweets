3c5ab53be7b25d93b32a9d9cb99a3e8bbb432c42
chanmuzi <chanmuzi@naver.com>
2023-03-22 17:43:13 +0900

    Fix : base_data_loader

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -1,22 +1,27 @@
 from torch.utils.data import DataLoader,random_split
 from .base_dataset import BaseDataset
 
-def BaseDataLoader(train_dataset,test_dataset,tokenizer,batch_size):
+def BaseDataLoader(dataset,tokenizer,batch_size,is_test):
     # BaseDataset(df,tokenizer,label,preprocess)
-    train_dataset = BaseDataset(train_dataset,tokenizer,label=True,preprocess=False)
-    test_dataset = BaseDataset(test_dataset,tokenizer,label=False,preprocess=False)
+    if is_test == True:
+        test_dataset = BaseDataset(dataset, tokenizer, label=False, preprocess=False)
+        test_loader = DataLoader(test_dataset, batchsize=batch_size, shuffle=False, pin_memory=False)
 
-    train_size = int(len(train_dataset)*0.8)
-    valid_size = len(train_dataset) - train_size
+        print(f'{len(test_dataset)} valid samples')
 
-    train_dataset,valid_dataset = random_split(train_dataset,[train_size,valid_size])
+        return test_loader
+    else:
+        train_dataset = BaseDataset(train_dataset,tokenizer,label=True,preprocess=False)
 
-    train_loader = DataLoader(train_dataset,batch_size=batch_size,shuffle=True,pin_memory=True)
-    valid_loader = DataLoader(valid_dataset,batch_size=batch_size,shuffle=False,pin_memory=True)
-    test_loader = DataLoader(test_dataset,batch_size=1,shuffle=False,pin_memory=False)
+        train_size = int(len(train_dataset)*0.8)
+        valid_size = len(train_dataset) - train_size
 
-    print(f'{len(train_dataset)} train samples')
-    print(f'{len(valid_dataset)} valid samples')
-    print(f'{len(test_dataset)} valid samples')
+        train_dataset,valid_dataset = random_split(train_dataset,[train_size,valid_size])
 
-    return train_loader,valid_loader,test_loader
+        train_loader = DataLoader(train_dataset,batch_size=batch_size,shuffle=True,pin_memory=True)
+        valid_loader = DataLoader(valid_dataset,batch_size=batch_size,shuffle=False,pin_memory=True)
+
+        print(f'{len(train_dataset)} train samples')
+        print(f'{len(valid_dataset)} valid samples')
+    
+        return train_loader,valid_loader,
